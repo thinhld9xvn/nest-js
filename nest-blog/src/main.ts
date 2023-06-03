7d011884f4735a04e49786bc019c5464d37293d6
Thinh Luu <thinhld9xvn@gmail.com>
2023-06-03 23:43:49 +0700

    update port

--- nest-blog/src/main.ts
+++ nest-blog/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     },
     templates: join(__dirname, '..', 'views'),
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
